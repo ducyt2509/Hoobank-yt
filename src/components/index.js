7192a7f72ffc3e1042437fbbe57ea983658789ae
ducyt2509 <duclee025@gmail.com>
2023-05-11 15:23:25 +0700

    Hoobank full

---
+++ src/components/index.js
@@ -0,0 +1,30 @@
+import Navbar from './Navbar'
+import Billing from './Billing'
+import Business from './Business'
+import Button from './Button'
+import Clients from './Clients'
+import CTA from './CTA'
+import Feedback from './Feedback'
+import GetStarted from './GetStarted'
+import Hero from './Hero'
+import Testimonials from './Testimonials'
+import Footer from './Footer'
+import CardDeal from './CardDeal'
+import Stats from './Stats'
+
+
+export {
+    Navbar,
+    Footer,
+    Billing,
+    Business,
+    Button,
+    Clients,
+    CTA,
+    Feedback,
+    GetStarted,
+    Hero,
+    Testimonials,
+    CardDeal,
+    Stats
+}
